837a81f4ee3905b98da880a2b50359044d0cb2a5
Klevis Hënkoja <113030599+KlevisHenkoja@users.noreply.github.com>
2024-02-03 12:30:08 +0100

    Update Listing.jsx

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -62,7 +62,7 @@ export default function Listing() {
             {listing.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className='h-[550px]'
+                  className='h-[550px] mx-7'
                   style={{
                     background: `url(${url}) center no-repeat`,
                     backgroundSize: 'cover',
